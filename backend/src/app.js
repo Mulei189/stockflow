c0e8b4bc3e9af20c8aeebd73cdd2985bbfda333c
Frank <frankmuema42@gmail.com>
2026-06-16 17:11:13 +0300

    Prettier and Eslint

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,18 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello and welcome to stockflow API')
+  res.send('Hello and welcome to stockflow API');
 });
 
 app.get('/api', (req, res) => {
-    res.status(200).json({message: 'Stockflow API is working'})
+  res.status(200).json({ message: 'Stockflow API is working' });
+});
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'StockFlow API is running',
+  });
 });
 
 export default app;
